a2d2341ce5dcc8d5a0fe57dfb9a667d0f58e059a
Isaac Dempsey <idempsey7@gmail.com>
2018-11-20 14:18:54 +0000

    Updated the d3 version, seperated the code into different files, put in some basic layout and styling

---
+++ d3_script.js
@@ -0,0 +1,211 @@
+// Define margins
+var margin = {
+    top: 50,
+    right: 20,
+    bottom: 25,
+    left: 40
+};
+
+// Width and height
+var outer_width = 700;
+var outer_height = 550;
+var svg_width = outer_width - margin.left - margin.right;
+var svg_height = outer_height - margin.top - margin.bottom;
+
+// The year to display
+display_year = 2008;
+
+// Define a function that filters data by year
+function yearFilter(value) {
+    return (value.Year == display_year)
+}
+
+// Create SVG element
+var svg = d3.select("main")
+    .append("svg")
+    .attr("width", outer_width)
+    .attr("height", outer_height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Define a function to draw a simple bar chart
+function generateVis() {
+
+    // Create scale functions
+    // Minimum and maximum of domain based on dataset fields
+    var xScale = d3.scaleLog()
+        .domain([d3.min(dataset, function(d) {
+            return +d.GDP;
+        }), d3.max(dataset, function(d) {
+            return +d.GDP;
+        })])
+        .range([1, svg_width]);
+
+    var yScale = d3.scaleLinear()
+        .domain([d3.min(dataset, function(d) {
+            return +d.Global_Competitiveness_Index;
+        }), d3.max(dataset, function(d) {
+            return +d.Global_Competitiveness_Index;
+        })])
+        .range([svg_height, 0]);
+
+    // Define scale for circle radius
+    var rScale = d3.scaleLinear()
+        .domain([d3.min(dataset, function(d) {
+            return +d.Population;
+        }), d3.max(dataset, function(d) {
+            return +d.Population;
+        })])
+        .range([1, 50]);
+
+    // Define axes
+    var xAxis = d3.axisBottom()
+        .scale(xScale)
+        .ticks(5);
+
+    var yAxis = d3.axisLeft()
+        .scale(yScale)
+        .ticks(5);
+
+    // Create and call the X-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + svg_height + ")")
+        .attr("id", "xAxis")
+        .call(xAxis);
+
+    // Create and call the Y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("id", "yAxis")
+        .call(yAxis);
+
+    // Filter the data to only include the current year
+    var filtered_datset = dataset.filter(yearFilter);
+
+
+    /******** PERFORM DATA JOIN ************/
+    var circles = svg.selectAll("circle")
+        .data(filtered_datset, function key(d) {
+            return d.Country;
+        });
+
+    // Create an empty div which will hold the country label
+    var countryLabel = d3.select("main")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .attr("id", "countryLabel");
+
+
+    /******** HANDLE UPDATE SELECTION ************/
+    // Update the display of existing elements to match new data
+    circles
+        .transition()
+        .duration(1000)
+        .attr("cx", function(d) {
+            return xScale(d.GDP);
+        })
+        .attr("cy", function(d) {
+            return yScale(d.Global_Competitiveness_Index);
+        })
+        .attr("r", function(d) {
+            return rScale(d.Population);
+        })
+        .style("opacity", "0.2")
+        .style("fill", "green");
+
+
+    /******** HANDLE ENTER SELECTION ************/
+    // Create new elements in the dataset
+    circles.enter()
+        .append("circle")
+        .attr("cx", function(d) {
+            return xScale(d.GDP);
+        })
+        .attr("cy", function(d) {
+            return yScale(d.Global_Competitiveness_Index);
+        })
+        .attr("r", function(d) {
+            return rScale(d.Population);
+        })
+        .style("opacity", "0.2")
+        .style("fill", "blue")
+        .on("mouseover", function(d) {
+            return countryLabel.style("visibility", "visible").text(d.Country);
+        })
+        .on("mousemove", function() {
+            return countryLabel.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+        })
+        .on("mouseout", function() {
+            return countryLabel.style("visibility", "hidden");
+        });
+
+    /******** HANDLE EXIT SELECTION ************/
+    // Remove circles that no longer have a matching data element
+    // TODO: setting visibility to null might not be needed
+    circles.exit()
+        .on("mouseover", function(d) {
+            return countryLabel.style("visibility", "hidden");
+        })
+        .on("mousemove", function(d) {
+            return countryLabel.style("visibility", "hidden");
+        })
+        .on("mouseout", function(d) {
+            return countryLabel.style("visibility", "hidden");
+        })
+        .remove();
+}
+
+
+/******** HANDLE SLIDER UPDATE ************/
+// Get necessary dom objects
+var slider = document.getElementById("yearRange");
+var sliderControlBtn = document.getElementById("yearRangeBtn");
+var yearDisplay = document.getElementById("year_header");
+
+// shows whether the animation is currently running or not
+var running = true;
+
+// updates the year and moves the slider to appropriate position
+function updateYearAndSlider() {
+    if (running) {
+
+        display_year = display_year + 1;
+        if (display_year > 2017) {
+            display_year = 2008;
+        }
+        slider.value = display_year;
+        yearDisplay.innerText = display_year;
+    }
+}
+
+// controls slider actions 
+slider.oninput = function() {
+    display_year = parseInt(this.value);
+    yearDisplay.innerText = display_year;
+    running = false;
+    sliderControlBtn.innerText = "Start";
+    generateVis();
+}
+
+// controls stop/start action of slider control button
+sliderControlBtn.onclick = function() {
+    running = !running;
+    sliderControlBtn.innerText = running ? "Stop" : "Start";
+}
+
+// Load the file data.csv and generate a visualisation based on it
+// Can use smaller selection of data in GCI_TestData.csv if needed
+d3.csv("./data/GCI_TestData.csv")
+    .then(function(data) {
+        dataset = data;
+        generateVis();
+        // Set up a callback so we iterate through the years
+        setInterval(function() {
+            updateYearAndSlider();
+            generateVis();
+        }, 1000);
+
+    });
